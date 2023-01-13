068bc8718b22845cbd61d7aeca1f13c556638091
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2023-01-13 15:05:09 -0500

    interaction toolkit + locomotiom

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
-    private InputDevice LeftController; 
-    private InputDevice RightController;
+    public InputActionReference ButtonInputs;
 
     private void Awake()
     {
@@ -19,48 +18,11 @@ public class InputManager : MonoBehaviour
         else Destroy(Instance);      
     }
     // Start is called before the first frame update
-    IEnumerator FindControllers()
-    {
-        List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
-        print("finding controllers");
-        if (devices.Count > 2)
-        {
-            print("found controllers");
-            LeftController = devices[1];
-            RightController = devices[2];
-            StartCoroutine(ControllerInput());
-            yield return null;
-        }
-        yield return new WaitForSeconds(0.1f);
-    }
 
-    IEnumerator ControllerInput()
-    {
-        while (true)
-        {
-            print("check controls");
-            LeftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonVal);
-            if (primaryButtonVal){
-                print("primary button pressed");
-            }
-            LeftController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryButtonVal);
-            if (primaryButtonVal)
-            {
-                print("secondary button pressed");
-            }
-            if (LeftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisVal))
-            {
-                print("Left Joystick val" + primary2DAxisVal);
-            }
-        }
-        print("done");
-        yield return null;
-
-    }
     void Start()
     {
-        StartCoroutine(FindControllers());
+        ButtonInputs.action.performed += Action_performed;
+
         //bind event with inputs 
 
         //joystick oculus 
@@ -73,9 +35,13 @@ public class InputManager : MonoBehaviour
         //vive joystick
     }
 
+    private void Action_performed(InputAction.CallbackContext obj)
+    {
+        print("heloo");
+    }
+
     private void Update()
     {
-        
 
     }
 }
