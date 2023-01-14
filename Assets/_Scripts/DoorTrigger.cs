279e15407710ea92b81a2f0a23dfee7b3d4518ca
Rylan Pozniak <tinkerguy1@gmail.com>
2023-01-14 11:49:50 -0800

    restaurant canvases interaction

---
+++ Assets/_Scripts/DoorTrigger.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public Transform creditsCanvas;
+
+    private void Start()
+    {
+        creditsCanvas.localScale = Vector3.zero;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (PlayerPrefs.GetInt("IsTeacher") == 0)
+        //{
+            if (other.gameObject.name == "Local VR Player")
+            {
+                Debug.Log("TRIGGER TRIGGERED");
+                Tween.LocalScale(creditsCanvas, new Vector3(0.01f, 0.01f, 0.01f), 0.5f, 0f);
+            }
+        //}
+    }
+
+    
+    private void OnTriggerExit(Collider other)
+    {
+        //if (PlayerPrefs.GetInt("IsTeacher") == 0)
+        //{
+            if (other.name == "Local VR Player")
+            {
+                Debug.Log("TRIGGER ALSO TRIGGERED");
+                Tween.LocalScale(creditsCanvas, Vector3.zero, 0.2f, 0f);
+            }
+        //}
+    }
+    
+}
