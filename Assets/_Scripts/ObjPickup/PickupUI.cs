ed8cc4aed462842daab67eed1bac4507c96f21e0
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2023-01-14 21:24:15 -0500

    apple example

--- Assets/_Scripts/ObjPickup/PickupUI.cs
+++ Assets/_Scripts/ObjPickup/PickupUI.cs
@@ -9,10 +9,18 @@ public class PickupUI : MonoBehaviour
     public TextMeshProUGUI hint2 = null;
     public TextMeshProUGUI hint3 = null;
 
+
     public void SetupUI(string h1,string h2,string h3)
     {
         hint1.text = h1;
         hint2.text = h2;
         hint3.text = h3;
+    }
+
+    public void Update()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        //transform.rotation.ToEuler().
+        
     }
 }
