5d175ed3977467341096f2124810815efc2ab121
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2023-01-14 19:41:43 -0500

    backend for held obj UI

---
+++ Assets/_Scripts/ObjPickup/PickupUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PickupUI : MonoBehaviour
+{
+    public TextMeshProUGUI hint1 = null;
+    public TextMeshProUGUI hint2 = null;
+    public TextMeshProUGUI hint3 = null;
+
+    void SetupUI(string h1,string h2,string h3)
+    {
+        hint1.text = h1;
+        hint2.text = h2;
+        hint3.text = h3;
+    }
+}
